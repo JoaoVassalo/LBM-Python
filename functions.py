bf0506ea00710353c2580bf94b1d699c264b9e77
JoaoVassalo <joaodvassalo@gmail.com>
2025-12-16 16:53:32 -0300

    Functioning code, using pop based

--- functions.py
+++ functions.py
@@ -1,21 +1,25 @@
+from constants import *
+
 def grid_id(x, y, Nx):
     return (x + Nx*y)
 
-def pop_id(x, y, i, Nx, Q):
-    return ((x+y*Nx)*Q + i)
+def pop_id(g_id, i, Q):
+    return (g_id*Q + i)
 
 def equilibrium(rho, w_i, a_s, ux, c_ix, uy, c_iy):
     return rho*w_i*( 1 + a_s**2*( ux*c_ix + uy*c_iy ) 
                     + a_s**4/2*( ( ux*c_ix + uy*c_iy )**2 
                                 - ( ux**2 + uy**2 )/a_s**2 ) )
 
-def m_xy_I(f_i, I_s, c_ix, c_iy, Q):
+def m_xy_I(f_i, I_s, c_ix, c_iy, Q, g_id):
     num = 0
     div = 0
+
     for i in I_s:
-        num += f_i * c_ix[i]*c_iy[i]
-    for i in range(Q):
-        div += f_i
+        p_id = pop_id(g_id, i, Q)
+        num += f_i[p_id] * c_ix[i]*c_iy[i]
+        div += f_i[p_id]
+
     return (num/div)
 
 def m_xy_north(m_xy_I, 
@@ -31,16 +35,19 @@ def m_xy_north(m_xy_I,
     Is_down = 0
     Os_down = 0
     Os_up = 0
+
     for i in I_s:
         Is_up += w_i[i]*c_ix[i]*c_iy[i]*(1 + 
                          a_s**2*u_max*c_ix[i] + 
                          a_s**4/2*u_max*(c_ix[i]**2 - 1/a_s**2))
         Is_down += w_i[i]*a_s**4*c_ix[i]**2*c_iy[i]**2
+
     for i in O_s:
         Os_down += w_i[i]*a_s**4*c_ix[i]*c_iy[i]
         Os_up += w_i[i]*(1 + 
-                         a_s**2*u_max*c_ix + 
+                         a_s**2*u_max*c_ix[i] + 
                          a_s**4/2*u_max**2*(c_ix[i]**2 - 1/a_s**2))
+        
     m_xy = (Is_up - m_xy_I*Os_up)/(m_xy_I*(1-omega)*Os_down - Is_down)
     return m_xy
 
@@ -57,11 +64,11 @@ def m_xy_wall(m_xy_I,
     Os_down = 0
     Os_up = 0
     for i in I_s:
-        Is_up += w_i[i]*c_ix*c_iy
-        Is_down += w_i[i]*a_s**4*c_ix**2*c_iy**2
+        Is_up += w_i[i]*c_ix[i]*c_iy[i]
+        Is_down += w_i[i]*a_s**4*c_ix[i]**2*c_iy[i]**2
     for i in O_s:
         Os_up += w_i[i]
-        Os_down += w_i[i]*a_s**4*c_ix*c_iy
+        Os_down += w_i[i]*a_s**4*c_ix[i]*c_iy[i]
     m_xy = (Is_up - m_xy_I*Os_up)/(m_xy_I*(1-omega)*Os_down - Is_down)
     return m_xy
     
@@ -69,21 +76,26 @@ def rho_north(m_xy,
               f_i, 
               c_ix, 
               c_iy, 
-              u_max, 
+              ux, 
               I_s, 
               O_s, 
               a_s, 
               w_i, 
-              omega):
+              omega,
+              Q,
+              g_id):
     rho_I_rho = 0
     rho_I = 0
+
     for i in O_s:
         rho_I_rho += (w_i[i]*(1 + 
-                             a_s**2*u_max*c_ix + 
-                             a_s**4/2*u_max**2*(c_ix[i]**2 - 1/a_s**2)) + 
+                             a_s**2*ux*c_ix[i] + 
+                             a_s**4/2*ux**2*(c_ix[i]**2 - 1/a_s**2)) + 
                              w_i[i]*(1-omega)*a_s**4*m_xy*c_ix[i]*c_iy[i])
+        
     for i in I_s:
-        rho_I += f_i[i]
+        p_id = pop_id(g_id, i, Q)
+        rho_I += f_i[p_id]
     
     rho = rho_I/rho_I_rho
     return rho
@@ -96,21 +108,64 @@ def rho_wall(m_xy,
              O_s, 
              a_s, 
              w_i, 
-             omega):
+             omega,
+             Q,
+             g_id):
     rho_I_rho = 0
     rho_I = 0
     for i in O_s:
-        rho_I_rho += (1-omega)*w_i*a_s**4*m_xy*c_ix*c_iy + w_i
+        rho_I_rho += (1-omega)*w_i[i]*a_s**4*m_xy*c_ix[i]*c_iy[i] + w_i[i]
     
     for i in I_s:
-        rho_I += f_i[i]
+        p_id = pop_id(g_id, i, Q)
+        rho_I += f_i[p_id]
     
     rho = rho_I/rho_I_rho
     return rho
 
-def rho_corner(f_i, I_s):
-    rho = 0
+def rho_corner(f_i, I_s, O_s, Q, g_id):
+    rho_I = 0
     for i in I_s:
-        rho += f_i[i]
+        p_id = pop_id(g_id, i, Q)
+        rho_I += f_i[p_id]
+    
+    sum_wi = 0
+    for i in O_s:
+        sum_wi += w_i[i]
+
+    return rho_I/sum_wi
+
+def calc_rho(f_i, Q, g_id):
+    rho = 0
+    for i in range(Q):
+        p_id = pop_id(g_id, i, Q)
+        rho += f_i[p_id]
+
     return rho
 
+def calc_velocity(f_i, c_ix, c_iy, Q, g_id, rho):
+    u_x = 0
+    u_y = 0
+    for i in range(Q):
+        p_id = pop_id(g_id, i, Q)
+        u_x += f_i[p_id]*c_ix[i]
+        u_y += f_i[p_id]*c_iy[i]
+    u_x *= 1/rho
+    u_y *= 1/rho
+
+    return u_x, u_y
+
+def calc_momentum(f_i, c_ix, c_iy, Q, g_id, rho, a_s):
+    m_xx = 0
+    m_yy = 0
+    m_xy = 0
+    for i in range(Q):
+        p_id = pop_id(g_id, i, Q)
+        m_xx += f_i[p_id]*(c_ix**2 - 1/(a_s**2))
+        m_yy += f_i[p_id]*(c_iy**2 - 1/(a_s**2))
+        m_xy += f_i[p_id]*(c_ix*c_iy)
+    m_xx *= 1/rho
+    m_yy *= 1/rho
+    m_xy *= 1/rho
+
+    return m_xx, m_yy, m_xy
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 #Importando as bibliotecas
-from numba import jit
 import numpy as np
 from constants import *
 from functions import *
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
 
 #Definindo o tamanho do grid
-Nx = 257
-Ny = 257
+Nx = 65
+Ny = 65
 grid_num = Nx*Ny
 
 #Definindo tempo de simulação.
-tf = 100
+tf = 10000
 t_interval = 10
 
 #Cálculo de tau.
-Re = 100
-u_max = 1
+Re = 1000
+u_max = 0.0256
 delta_t = 1
 
 ni = u_max*Ny/Re
@@ -28,6 +29,10 @@ omega = tau**(-1)
 rho = np.ones(grid_num)
 ux = np.zeros(grid_num)
 uy = np.zeros(grid_num)
+mxx = np.zeros(grid_num)
+myy = np.zeros(grid_num)
+mxy = np.zeros(grid_num)
+
 
 rho_plot = []
 ux_plot = []
@@ -40,44 +45,251 @@ for x in range(Nx):
     for y in range(Ny):
         for i in range(Q):
             g_id = grid_id(x, y, Nx)
-            f_eq[pop_id(x, y, i, Nx, Q)] = equilibrium(rho[g_id], w_i[i], a_s, ux[g_id], 
+            f_eq[pop_id(g_id, i, Q)] = equilibrium(rho[g_id], w_i[i], a_s, ux[g_id], 
                                                        c_ix[i], uy[g_id], c_iy[i])
 
-for y in range(Ny):
+f_i = np.copy(f_eq)
+
+col = np.zeros(len(f_i))
+f_col = np.zeros(len(f_i))
+
+for t in range(tf):
+    
+
+    #Colisão.
     for x in range(Nx):
-        print(x, y)
-        if x==0 and y==0:
-            print("Sudoeste")
-            I_s = [0, 1, 2, 5]
-            O_s = [0, 1, 2, 3, 4, 6, 7, 8]
-        elif x==(Nx-1) and y==0:
-            print("Sudeste")
-            I_s = [0, 2, 3, 6]
-            O_s = [0, 1, 2, 3, 4, 5, 7, 8]
-        elif x==0 and y==(Ny-1):
-            print("Noroeste")
-            I_s = [0, 1, 4, 8]
-            O_s = [0, 1, 2, 3, 4, 5, 6, 7]
-        elif x==(Nx-1) and y==(Ny-1):
-            print("Nordeste")
-            I_s = [0, 3, 4, 7]
-            O_s = [0, 1, 2, 3, 4, 5, 6, 8]
-        elif y==0:
-            print("Sul")
-            I_s = [0, 1, 2, 3, 5, 6]
-            O_s = [0, 1, 3, 4, 7, 8]
-        elif y==(Ny-1):
-            print("Norte")
-            I_s = [0, 1, 3, 4, 7, 8]
-            O_s = [0, 1, 2, 3, 5, 6]
-        elif x==0:
-            print("Oeste")
-            I_s = [0, 1, 2, 4, 5, 8]
-            O_s = [0, 2, 3, 4, 6, 7]
-        elif x==(Nx-1):
-            print("Leste")
-            I_s = [0, 2, 3, 4, 6, 7]
-            O_s = [0, 1, 2, 4, 5, 8]
-        else:
-            print("Centro")
-            I_s = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        for y in range(Ny):
+            g_id = grid_id(x, y, Nx)
+            for i in range(Q):
+                p_id = pop_id(g_id, i, Q)
+                col[p_id] = - (f_i[p_id] - f_eq[p_id])/tau
+                f_col[p_id] = f_i[p_id] + col[p_id]
+
+    #Propagação.
+    for x in range(Nx):
+        for y in range(Ny):
+            for i in range(Q):
+
+                x_to = (x + c_ix[i] + Nx) % Nx   #Contornos periódicos.
+                y_to = (y + c_iy[i] + Ny) % Ny
+
+
+                idx_to   = pop_id(grid_id(x_to, y_to, Nx), i, Q)
+                idx_from = pop_id(grid_id(x, y, Nx), i, Q)
+
+                f_i[idx_to] = f_col[idx_from]
+
+    for x in range(Nx):
+        for y in range(Ny):
+            g_id = grid_id(x, y, Nx)
+
+            if x==0 and y==0:   #Sudoeste
+                I_s = [0, 3, 4, 7]
+                O_s = [0, 1, 2, 5]
+                rho[g_id] = rho_corner(f_i, I_s, O_s, Q, g_id)
+                ux[g_id] = 0
+                uy[g_id] = 0
+                mxy = 0
+
+                for i in range(Q):
+                    p_id = pop_id(g_id, i, Q)
+                    f_i[p_id] = w_i[i] * rho[g_id] * (1 + 
+                                                      a_s**2 * (ux[g_id]*c_ix[i] + uy[g_id]*c_iy[i]) + 
+                                                      a_s**4/2 * (ux[g_id]**2*(c_ix[i]**2 - 1/a_s**2) + uy[g_id]**2*(c_iy[i]**2 - 1/a_s**2)) + 
+                                                      a_s**4 * (mxy*c_ix[i]*c_iy[i]) )
+
+            elif x==(Nx-1) and y==0:    #Sudeste
+                I_s = [0, 1, 4, 8]
+                O_s = [0, 2, 3, 6]
+                rho[g_id] = rho_corner(f_i, I_s, O_s, Q, g_id)
+                ux[g_id] = 0
+                uy[g_id] = 0
+                mxy = 0
+
+                for i in range(Q):
+                    p_id = pop_id(g_id, i, Q)
+                    f_i[p_id] = w_i[i] * rho[g_id] * (1 + 
+                                                      a_s**2 * (ux[g_id]*c_ix[i] + uy[g_id]*c_iy[i]) + 
+                                                      a_s**4/2 * (ux[g_id]**2*(c_ix[i]**2 - 1/a_s**2) + uy[g_id]**2*(c_iy[i]**2 - 1/a_s**2)) + 
+                                                      a_s**4 * (mxy*c_ix[i]*c_iy[i]) )
+
+            elif x==0 and y==(Ny-1):    #Noroeste
+                I_s = [0, 2, 3, 6]
+                O_s = [0, 1, 4, 8]
+                ux[g_id] = u_max
+                uy[g_id] = 0
+                mxy_I = m_xy_I(f_i, I_s, c_ix, c_iy, Q, g_id)
+                mxy = m_xy_north(mxy_I, c_ix, c_iy, ux[g_id], I_s, O_s, a_s, w_i, omega)
+                rho[g_id] = rho_north(mxy, f_i, c_ix, c_iy, ux[g_id], I_s, O_s, a_s, w_i, omega, Q, g_id)
+
+                for i in range(Q):
+                    p_id = pop_id(g_id, i, Q)
+                    f_i[p_id] = w_i[i] * rho[g_id] * (1 + 
+                                                      a_s**2 * (ux[g_id]*c_ix[i] + uy[g_id]*c_iy[i]) + 
+                                                      a_s**4/2 * (ux[g_id]**2*(c_ix[i]**2 - 1/a_s**2) + uy[g_id]**2*(c_iy[i]**2 - 1/a_s**2)) + 
+                                                      a_s**4 * (mxy*c_ix[i]*c_iy[i]) )
+
+            elif x==(Nx-1) and y==(Ny-1):   #Nordeste
+                I_s = [0, 1, 2, 5]
+                O_s = [0, 3, 4, 7]
+                ux[g_id] = u_max
+                uy[g_id] = 0
+                mxy_I = m_xy_I(f_i, I_s, c_ix, c_iy, Q, g_id)
+                mxy = m_xy_north(mxy_I, c_ix, c_iy, ux[g_id], I_s, O_s, a_s, w_i, omega)
+                rho[g_id] = rho_north(mxy, f_i, c_ix, c_iy, ux[g_id], I_s, O_s, a_s, w_i, omega, Q, g_id)
+
+                for i in range(Q):
+                    p_id = pop_id(g_id, i, Q)
+                    f_i[p_id] = w_i[i] * rho[g_id] * (1 + 
+                                                      a_s**2 * (ux[g_id]*c_ix[i] + uy[g_id]*c_iy[i]) + 
+                                                      a_s**4/2 * (ux[g_id]**2*(c_ix[i]**2 - 1/a_s**2) + uy[g_id]**2*(c_iy[i]**2 - 1/a_s**2)) + 
+                                                      a_s**4 * (mxy*c_ix[i]*c_iy[i]) )
+
+
+            elif y==0:  #Sul
+                I_s = [0, 1, 3, 4, 7, 8]
+                O_s = [0, 1, 2, 3, 5, 6]
+                mxy_I = m_xy_I(f_i, I_s, c_ix, c_iy, Q, g_id)
+                ux[g_id] = 0
+                uy[g_id] = 0
+                mxy = m_xy_wall(mxy_I, c_ix, c_iy, I_s, O_s, a_s, w_i, omega)
+                rho[g_id] = rho_wall(mxy, f_i, c_ix, c_iy, I_s, O_s, a_s, w_i, omega, Q, g_id)
+
+                for i in range(Q):
+                    p_id = pop_id(g_id, i, Q)
+                    f_i[p_id] = w_i[i] * rho[g_id] * (1 + 
+                                                      a_s**2 * (ux[g_id]*c_ix[i] + uy[g_id]*c_iy[i]) + 
+                                                      a_s**4/2 * (ux[g_id]**2*(c_ix[i]**2 - 1/a_s**2) + uy[g_id]**2*(c_iy[i]**2 - 1/a_s**2)) + 
+                                                      a_s**4 * (mxy*c_ix[i]*c_iy[i]) )
+
+            elif y==(Ny-1): #Norte
+                I_s = [0, 1, 2, 3, 5, 6]
+                O_s = [0, 1, 3, 4, 7, 8]
+                mxy_I = m_xy_I(f_i, I_s, c_ix, c_iy, Q, g_id)
+                ux[g_id] = u_max
+                uy[g_id] = 0
+                mxy = m_xy_north(mxy_I, c_ix, c_iy, ux[g_id], I_s, O_s, a_s, w_i, omega)
+                rho[g_id] = rho_north(mxy, f_i, c_ix, c_iy, ux[g_id], I_s, O_s, a_s, w_i, omega, Q, g_id)
+
+                for i in range(Q):
+                    p_id = pop_id(g_id, i, Q)
+                    f_i[p_id] = w_i[i] * rho[g_id] * (1 + 
+                                                      a_s**2 * (ux[g_id]*c_ix[i] + uy[g_id]*c_iy[i]) + 
+                                                      a_s**4/2 * (ux[g_id]**2*(c_ix[i]**2 - 1/a_s**2) + uy[g_id]**2*(c_iy[i]**2 - 1/a_s**2)) + 
+                                                      a_s**4 * (mxy*c_ix[i]*c_iy[i]) )
+
+            elif x==0:  #Oeste
+                I_s = [0, 2, 3, 4, 6, 7]
+                O_s = [0, 1, 2, 4, 5, 8]
+                mxy_I = m_xy_I(f_i, I_s, c_ix, c_iy, Q, g_id)
+                ux[g_id] = 0
+                uy[g_id] = 0
+                mxy = m_xy_wall(mxy_I, c_ix, c_iy, I_s, O_s, a_s, w_i, omega)
+                rho[g_id] = rho_wall(mxy, f_i, c_ix, c_iy, I_s, O_s, a_s, w_i, omega, Q, g_id)
+
+                for i in range(Q):
+                    p_id = pop_id(g_id, i, Q)
+                    f_i[p_id] = w_i[i] * rho[g_id] * (1 + 
+                                                      a_s**2 * (ux[g_id]*c_ix[i] + uy[g_id]*c_iy[i]) + 
+                                                      a_s**4/2 * (ux[g_id]**2*(c_ix[i]**2 - 1/a_s**2) + uy[g_id]**2*(c_iy[i]**2 - 1/a_s**2)) + 
+                                                      a_s**4 * (mxy*c_ix[i]*c_iy[i]) )
+
+            elif x==(Nx-1): #Leste
+                I_s = [0, 1, 2, 4, 5, 8]
+                O_s = [0, 2, 3, 4, 6, 7]
+                mxy_I = m_xy_I(f_i, I_s, c_ix, c_iy, Q, g_id)
+                ux[g_id] = 0
+                uy[g_id] = 0
+                mxy = m_xy_wall(mxy_I, c_ix, c_iy, I_s, O_s, a_s, w_i, omega)
+                rho[g_id] = rho_wall(mxy, f_i, c_ix, c_iy, I_s, O_s, a_s, w_i, omega, Q, g_id)
+
+                for i in range(Q):
+                    p_id = pop_id(g_id, i, Q)
+                    f_i[p_id] = w_i[i] * rho[g_id] * (1 + 
+                                                      a_s**2 * (ux[g_id]*c_ix[i] + uy[g_id]*c_iy[i]) + 
+                                                      a_s**4/2 * (ux[g_id]**2*(c_ix[i]**2 - 1/a_s**2) + uy[g_id]**2*(c_iy[i]**2 - 1/a_s**2)) + 
+                                                      a_s**4 * (mxy*c_ix[i]*c_iy[i]) )
+
+            else:   #Centro
+                rho[g_id] = calc_rho(f_i, Q, g_id)
+                ux[g_id], uy[g_id] = calc_velocity(f_i, c_ix, c_iy, Q, g_id, rho[g_id])
+
+
+
+    for x in range(Nx):
+        for y in range(Ny):
+            for i in range(Q):
+                g_id = grid_id(x, y, Nx)
+                f_eq[pop_id(g_id, i, Q)] = equilibrium(rho[g_id], w_i[i], a_s, ux[g_id], 
+                                                        c_ix[i], uy[g_id], c_iy[i])
+
+
+    if t % t_interval == 0:
+        print("---------------------------------------", t, "---------------------------------------")
+        rho_plot.append(rho.copy())
+        ux_plot.append(ux.copy())
+        uy_plot.append(uy.copy())
+
+# Cria a figura e o eixo
+fig, ax = plt.subplots()
+img = ax.imshow(np.zeros((Ny, Nx)), origin='lower', cmap='viridis',
+                vmin=np.min(rho_plot), vmax=np.max(rho_plot))
+ax.set_title("Densidade ρ ao longo do tempo")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+
+cb = plt.colorbar(img, ax=ax)
+cb.set_label("ρ")
+
+# Função que atualiza cada frame
+def update(t):
+    rho_t = np.array(rho_plot[t]).reshape((Ny, Nx))
+    img.set_data(rho_t)
+    ax.set_title(f"Densidade ρ — passo {t}")
+    return [img]
+
+# Cria a animação
+ani = FuncAnimation(fig, update, frames=len(rho_plot), interval=100, blit=True)
+
+# Caminho onde salvar o gif
+output_path = r"C:\Users\08552591910\Pictures\Attempt_2.gif"
+
+# Salva o GIF (usa PillowWriter)
+writer = PillowWriter(fps=10)  # ajuste fps se quiser
+ani.save(output_path, writer=writer, dpi=150)
+
+print(f"GIF salvo em: {output_path}")
+plt.close()
+
+# Cria a figura e eixo
+fig, ax = plt.subplots(figsize=(6,6))
+
+# Inicializa com zeros
+img = ax.imshow(np.zeros((Ny, Nx)), origin='lower', cmap='viridis',
+                vmin=0, vmax=u_max)  # ajusta vmin/vmax se souber os limites
+ax.set_title("Magnitude da velocidade |u|")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+cb = plt.colorbar(img, ax=ax)
+cb.set_label("|u|")
+
+# Função de atualização
+def update(t):
+    U = np.array(ux_plot[t]).reshape((Ny, Nx))
+    V = np.array(uy_plot[t]).reshape((Ny, Nx))
+    magnitude = np.sqrt(U**2 + V**2)
+    
+    img.set_data(magnitude)
+    ax.set_title(f"Magnitude da velocidade — passo {t}")
+    return [img]
+
+# Cria a animação
+ani = FuncAnimation(fig, update, frames=len(rho_plot), interval=100, blit=True)
+
+# Salva em GIF
+output_path = r"C:\Users\08552591910\Pictures\Velocity_Field_Magnitude.gif"
+writer = PillowWriter(fps=10)
+ani.save(output_path, writer=writer, dpi=150)
+
+print(f"GIF salvo em: {output_path}")
+plt.close()
